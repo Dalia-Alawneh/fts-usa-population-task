5c4ed94513f3b3afe35170f620405a2a1269f534
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-03-18 11:59:48 +0200

    Display Data

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const API_URL = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population'
+
+const getPopulationData = async () => {
+  try{
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return []
+  }catch(e){
+    console.error("Error", e.message)
+  }
+}
+
+const renderPopulationData = (data) => {
+  return data.map(item => `
+    <tr class="border-b border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors">
+      <td class="py-3 px-6">${item["ID Nation"]}</td>
+      <td class="py-3 px-6">${item["ID Year"]}</td>
+      <td class="py-3 px-6">${item["Population"]}</td>
+      <td class="py-3 px-6">${item["Nation"]}</td>
+    </tr>
+    `).join('');
+}
+
+const showDataTable = async () => {
+  const { data } = await getPopulationData();
+  const populationTable = document.getElementById('populations')
+  if(data) {
+    populationTable.innerHTML = renderPopulationData(data)
+  }else{
+    populationTable.innerHTML= '<td colspan="5" class="py-4 w-full text-center">No Data Found</td>'
+  }
+}
+
+showDataTable();
